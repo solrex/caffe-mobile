3009971dbaa465b96fef5981967564b5177a3675
Wenbo Yang <solrex@gmail.com>
2017-02-03 17:03:07 +0800

    Add empty ios project.

---
+++ examples/ios/simple/CaffeSimple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaffeSimple
+//
+//  Created by Wenbo Yang on 2017/2/3.
+//  Copyright © 2017年 com.yangwenbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examples/ios/simple/CaffeSimple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaffeSimple
+//
+//  Created by Wenbo Yang on 2017/2/3.
+//  Copyright © 2017年 com.yangwenbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
