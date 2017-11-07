0e9557fd2beba49564972524eb5cc3c85516040c
Wenbo Yang <solrex@gmail.com>
2017-11-06 20:07:29 -0800

    Merge x86 and arm to a single .a.

--- tools/build_ios.sh
+++ tools/build_ios.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
+# Exit on any error
 
-cd ./third_party
+PROJECT_ROOT=$PWD
+
+function merge-Protobuf {
+    echo "$(tput setaf 2)"
+    echo "#####################"
+    echo " Merging .a for iPhoneSimulator and iPhoneOS"
+    echo "#####################"
+    echo "$(tput sgr0)"
+    set -x
+    rm -rf ios-protobuf
+    cp -r iPhoneOS-protobuf ios-protobuf
+    rm -rf ios-protobuf/lib/*
+    lipo -create iPhoneSimulator-protobuf/lib/libprotobuf-lite.a iPhoneOS-protobuf/lib/libprotobuf-lite.a -output ios-protobuf/lib/libprotobuf-lite.a
+    lipo -create iPhoneSimulator-protobuf/lib/libprotobuf.a iPhoneOS-protobuf/lib/libprotobuf.a -output ios-protobuf/lib/libprotobuf.a
+    set +x
+}
+
+cd $PROJECT_ROOT/third_party
 ./build-protobuf-3.1.0.sh iPhoneOS
-mkdir ../build
-cd ../build
+./build-protobuf-3.1.0.sh iPhoneSimulator
+merge-Protobuf
+
+mkdir -p $PROJECT_ROOT/build_iPhoneOS
+cd $PROJECT_ROOT/build_iPhoneOS
 rm -rf *
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../third_party/ios-cmake/toolchain/iOS.cmake \
     -DIOS_PLATFORM=OS
 make -j 4
+
+mkdir -p $PROJECT_ROOT/build_iPhoneSimulator
+cd $PROJECT_ROOT/build_iPhoneSimulator
+rm -rf *
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../third_party/ios-cmake/toolchain/iOS.cmake \
+    -DIOS_PLATFORM=SIMULATOR
+make -j 4
+
+cd $PROJECT_ROOT
+set -x
+rm -rf build_ios
+mkdir -p build_ios/lib
+cp -r build_iPhoneOS/include build_ios/include
+lipo -create build_iPhoneOS/lib/libcaffe.a build_iPhoneSimulator/lib/libcaffe.a -output build_ios/lib/libcaffe.a
+lipo -create build_iPhoneOS/lib/libproto.a build_iPhoneSimulator/lib/libproto.a -output build_ios/lib/libproto.a
+set +x
